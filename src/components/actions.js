1963789bee019de1471061f8bb06ffa626896de8
Kalyan3081 <vijaykalyan3081@gmail.com>
2024-12-23 17:13:46 +0530

    c

--- src/components/actions.js
+++ src/components/actions.js
@@ -1,19 +1,11 @@
-export const FETCH_LOREM_LOADING = "FETCH_LOREM_LOADING";
-export const FETCH_LOREM_SUCCESS = "FETCH_LOREM_SUCCESS";
-export const FETCH_LOREM_FAILURE = "FETCH_LOREM_FAILURE";
+const UPDATE = 'UPDATE';
 
-export const fetchLorem = () => {
-    return async (dispatch) => {
-        dispatch({ type: FETCH_LOREM_LOADING });
-
-        try {
-            const res = await fetch("https://jsonplaceholder.typicode.com/posts")
-            const data = await res.json()
-            // console.log(data)
-            dispatch({ type: FETCH_LOREM_SUCCESS, payload: data });
-        } catch (error) {
-            dispatch({ type: FETCH_LOREM_FAILURE, payload: error.message });
-            // dispatch({ type: FETCH_LOREM_FAILURE, payload: error })
-        }
-    }
+const UpdatePost = (post)=>
+{
+  return {
+    type:UPDATE,
+    post
+  }
 }
+
+export default UpdatePost;
